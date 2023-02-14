16920f7d8702102bc7da9c0bf3ce4b3bc2b13311
vasiliev-alexey <alex_v81@MAIL.RU>
2023-02-14 11:42:37 +0300

    finish

--- src/app/table-module/reusable-table-dumb/reusable-table-dumb.component.ts
+++ src/app/table-module/reusable-table-dumb/reusable-table-dumb.component.ts
@@ -1,17 +1,43 @@
-import {Component, Input} from '@angular/core';
-import {ReusableTableDumbColumnsInterface} from './reusable-table-dumb.interfaces';
+import {
+  ChangeDetectorRef,
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
+import { ReusableTableDumbColumnsInterface } from './reusable-table-dumb.interfaces';
 
 @Component({
   selector: 'app-reusable-table-dumb',
   templateUrl: './reusable-table-dumb.component.html',
-  styleUrls: ['./reusable-table-dumb.component.less']
+  styleUrls: ['./reusable-table-dumb.component.less'],
 })
-export class ReusableTableDumbComponent<T extends Record<any, any>>  {
+export class ReusableTableDumbComponent<T extends Record<any, any>>
+  implements OnChanges
+{
   @Input()
-  public columns: ReusableTableDumbColumnsInterface<T> = []
+  public columns: ReusableTableDumbColumnsInterface<T> = [];
 
   @Input()
-  public data: T[] = []
+  public data: T[] = [];
 
+  #originColumns: ReusableTableDumbColumnsInterface<T> = [];
+  cell1?: T = undefined;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'].firstChange) {
+      this.#originColumns = [...this.columns];
+    }
+    let existingCols: string[] = [];
+
+    if (this.data.length) {
+      existingCols = Object.keys(this.data[0]);
+    }
+    this.columns = this.#originColumns.filter((cd) =>
+      existingCols.find((col) => col === cd.key)
+    );
+  }
 }
--- src/app/table-module/some-parent-component/some-parent-component.component.ts
+++ src/app/table-module/some-parent-component/some-parent-component.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import {
-  ReusableTableDumbColumnsInterface
-} from '../reusable-table-dumb/reusable-table-dumb.interfaces';
+import { ReusableTableDumbColumnsInterface } from '../reusable-table-dumb/reusable-table-dumb.interfaces';
+import { faker } from '@faker-js/faker';
+import * as moment from 'moment';
 
 interface UserInterface {
-  firstName: string
-  lastName: string
-  middleName?: string
-  birthDate: string
+  firstName: string;
+  lastName: string;
+  middleName?: string;
+  birthDate: string;
 }
+
+const format = 'DD.MM.yyyy';
 
 class User implements UserInterface {
   constructor(
@@ -16,47 +18,78 @@ class User implements UserInterface {
     public readonly lastName = 'Unknown',
     public readonly middleName = '',
     public readonly birthDate = 'xx.xx.xxxx'
-  ) {
-  }
-}
-interface MegaUserInterface extends UserInterface {
-  hobbies: string[]
+  ) {}
 }
 
-class MegaUser extends User implements MegaUserInterface{
+interface MegaUserInterface extends UserInterface {
+  hobbies: string[];
+}
+
+class MegaUser extends User implements MegaUserInterface {
   constructor(
     firstName = 'Unknown',
     lastName = 'Unknown',
     middleName = '',
     birthDate = 'xx.xx.xxxx',
-    public readonly hobbies = ['no hobbies'],
+    public readonly hobbies = ['no hobbies']
   ) {
-    super(firstName, lastName, middleName, birthDate)
+    super(firstName, lastName, middleName, birthDate);
   }
 }
+
+type CommonDataUserInterface = Partial<MegaUserInterface>;
 
 @Component({
   selector: 'app-some-parent-component',
   templateUrl: './some-parent-component.component.html',
 })
 export class SomeParentComponentComponent {
-  public readonly columns: ReusableTableDumbColumnsInterface<UserInterface> = [
-    { title: 'First name', key: 'firstName'},
-    { title: 'Last name', key: 'lastName'},
-    { title: 'Middle name', key: 'middleName'},
-    { title: 'Age', key: 'birthDate'},
-  ]
+  public readonly columns: ReusableTableDumbColumnsInterface<CommonDataUserInterface> =
+    [
+      { title: 'First name', key: 'firstName' },
+      { title: 'Last name', key: 'lastName' },
+      { title: 'Middle name', key: 'middleName' },
+      { title: 'Age', key: 'birthDate' },
+      { title: 'hobbies', key: 'hobbies' },
+    ];
 
-  public readonly commonUsers: UserInterface[] = [
-    new User('Igor','Miyagi', 'Alekseevich', '20.01.1976'),
-    new User()
-  ]
+  public readonly commonUsers = generateUser();
+  public readonly megaUsers = generateUser('mega') as MegaUserInterface[];
 
-  public readonly megaUsers: MegaUserInterface[] = [
-    new MegaUser(
-      'Igor','Miyagi', 'Alekseevich', '20.01.1976',
-      ['football', 'flowers']
-    ),
-    new MegaUser()
-  ]
+  isUser: boolean = true;
+
+  onItemChange() {
+    this.isUser = !this.isUser;
+  }
 }
+
+const generateUser = (
+  type: 'user' | 'mega' = 'user'
+): Array<UserInterface> | Array<MegaUserInterface> => {
+  const result = [];
+
+  for (let i = 0; i < 7; i++) {
+    if (i % 3) {
+      result.push(new User());
+    }
+
+    let user =
+      type === 'user'
+        ? new User(
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.name.middleName(),
+            moment(faker.date.birthdate()).format(format)
+          )
+        : new MegaUser(
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.name.middleName(),
+            moment(faker.date.birthdate()).format(format),
+            [faker.word.verb(), faker.word.verb()]
+          );
+    result.push(user);
+  }
+
+  return result;
+};
